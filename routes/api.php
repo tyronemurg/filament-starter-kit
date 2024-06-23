cdd2c72d9b789f78ea60123c91178945d3b8850a
tyronemurg <tyronemurg@gmail.com>
2024-06-23 18:04:06 +0200

    Category API Resource

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ProductController;
 use App\Http\Controllers\API\PostController;
+use App\Http\Controllers\API\CategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -28,3 +29,6 @@ Route::apiResource('products', ProductController::class);
 
 //Posts API Routes
 Route::apiResource('posts', PostController::class);
+
+//Categories API Routes
+Route::apiResource('categories', CategoryController::class);
