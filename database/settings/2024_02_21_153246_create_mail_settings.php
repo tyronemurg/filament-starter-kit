53a3343826dee95e28a89af16e56f605d009be44
tyronemurg <tyronemurg@gmail.com>
2024-07-14 18:27:35 +0200

    App Configurations

--- database/settings/2024_02_21_153246_create_mail_settings.php
+++ database/settings/2024_02_21_153246_create_mail_settings.php
@@ -7,7 +7,7 @@ return new class extends SettingsMigration
     public function up(): void
     {
         $this->migrator->add('mail.from_address', 'no-reply@starter-kit.com');
-        $this->migrator->add('mail.from_name', 'SuperDuper Starter Kit');
+        $this->migrator->add('mail.from_name', 'Web Warrior Filament Starter Kit');
         $this->migrator->add('mail.driver', 'smtp');
         $this->migrator->add('mail.host', null);
         $this->migrator->add('mail.port', 587);
