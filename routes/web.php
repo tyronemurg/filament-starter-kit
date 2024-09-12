614fcfb6c1c3c39cfcc9c84b281f9560930b724d
tyronemurg <tyronemurg@gmail.com>
2024-09-12 12:24:48 +0200

    Heatmap Route Change

--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/heatmap', [NewHeatMapPage::class, 'show']);
+Route::get('/admin/heatmap', [NewHeatMapPage::class, 'show']);
